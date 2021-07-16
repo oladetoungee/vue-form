da6b742f24ef527323bbf2de7230073ca8dafc19
unknown <demilad1998@gmaill.com>
2021-07-16 13:16:46 +0100

    working with form in vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,124 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Add Blog Post</h1>
+
+                <form @submit.prevent="isSubmitted= true" >
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" class="form-control" id="title" v-model="post.title">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="content">Content</label>
+                        <textarea class="form-control" id="content" v-model.lazy="post.content" cols="30" rows="10"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="checkbox" v-model="post.publishImmediately" id="publish-immediately" >
+                        <label for="publish-immediately">Publish Immediately</label>
+                    </div>
+                    <div class="form-group">
+                        <label>Share on</label>
+
+                        <div v-for="media in formData.socialMedia" :key="media" >
+                            <input type="checkbox" id="media" :value="media" v-model="post.shareOn">
+                            <label :for="media">{{ media }}</label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Category</label>
+
+                        <div v-for="category in formData.categories" :key="category" >
+                            <input type="radio" id="category" :value="category" v-model="post.category">
+                            <label :for="category">{{ category }}</label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control" id="select-series" v-model="post.series">
+                            <option value="">Choose Series</option>
+                            <option v-for="series in formData.series" :key="series" :value="series">{{ series }}</option>
+                        </select>
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Publish">
+                </form>
+
+                <hr>
+
+                <table class="table table-striped" v-if="isSubmitted">
+                    <thead>
+                        <tr>
+                            <td class="col-xs-6"><strong>Field</strong></td>
+                            <td class="col-xs-6"><strong>Value</strong></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Title</td>
+                            <td>{{ post.title }}</td>
+                        </tr>
+                        <tr>
+                            <td>Content</td>
+                            <td style="white-space: pre;">{{ post.content }}</td>
+                        </tr>
+                        <tr>
+                            <td>Publish immediately</td>
+                            <td>{{ post.publishImmediately }}</td>
+                        </tr>
+                        <tr>
+                            <td>Share on</td>
+                            <td>
+                                <ul>
+                                    <li v-for="post in post.shareOn" :key="post">{{post}}</li>
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Category</td>
+                            <td>{{ post.category }}</td>
+                        </tr>
+                        <tr>
+                            <td>Series</td>
+                            <td>{{ post.series }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+                isSubmitted: false,
+                media: "",
+                post: {
+                    title: '',
+                    content: '',
+                    publishImmediately: true,
+                    shareOn: ['Facebook'],
+                    category: 'Frontend',
+                    series: ''
+                },
+                formData: {
+                    socialMedia: ['Facebook', 'Twitter'],
+                    categories: ['Frontend', 'Backend'],
+                    series: [
+                        'Vue.js: From Beginner to Professional',
+                        'Complete Guide to Elasticsearch'
+                    ]
+                }
+            };
+        }
+    }
 </script>
+
+<style>
+    input[type="radio"] + label,
+    input[type="checkbox"] + label {
+        font-weight: normal;
+    }
+</style>
